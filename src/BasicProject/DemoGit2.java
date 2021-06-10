f1b1b4e0300988d1bff8b5633a120a234ad8c70b
Admin <Admin@pc>
2021-06-10 13:53:13 +0530

    Commiting Test2 to BranchX

---
+++ src/BasicProject/DemoGit2.java
@@ -0,0 +1,22 @@
+package BasicProject;
+
+public class DemoGit2 {
+
+	public static void main(String[] args) 
+	{
+		
+		String hungry="Yes";
+		
+		if(hungry=="Yes")
+		{
+			System.out.println("eat Something");
+		}
+		else
+		{
+			System.out.println("Dont work");
+		}
+		
+
+	}
+
+}
