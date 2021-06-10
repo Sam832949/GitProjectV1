8e1dfebce1b56a802adfcdb2cc26db7a4ed88676
Admin <Admin@pc>
2021-06-10 11:26:41 +0530

    First Commit

---
+++ src/BasicProject/DemoGit.java
@@ -0,0 +1,22 @@
+package BasicProject;
+
+public class DemoGit {
+
+	public static void main(String[] args) 
+	{
+		
+		String hungry="Yes";
+		
+		if(hungry=="Yes")
+		{
+			System.out.println("eat Something");
+		}
+		else
+		{
+			System.out.println("Dont work");
+		}
+		
+
+	}
+
+}
